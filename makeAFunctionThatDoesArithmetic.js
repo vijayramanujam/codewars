672d2f604f8f2ae878a05d8135a16e67fbd62755
Vijay Ramanujam <159724240+vijayramanujam@users.noreply.github.com>
2024-07-22 23:15:58 +0530

    Make a function that does arithmetic!

---
+++ makeAFunctionThatDoesArithmetic.js
@@ -0,0 +1,17 @@
+function arithmetic(a, b, operator) {
+
+    switch (operator) {
+
+        case 'add':
+            return a + b
+            break
+        case 'subtract':
+            return a - b
+            break
+        case 'divide':
+            return a / b
+            break
+        case 'multiply':
+            return a * b
+    }
+}
