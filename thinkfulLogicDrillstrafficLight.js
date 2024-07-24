cd46d661ce71959c2b0e50a69e5c35251ca68baa
Vijay Ramanujam <159724240+vijayramanujam@users.noreply.github.com>
2024-07-24 22:24:48 +0530

    Thinkful - Logic Drills: Traffic light

---
+++ thinkfulLogicDrillstrafficLight.js
@@ -0,0 +1,22 @@
+function updateLight(current) {
+
+    switch (current) {
+
+        case 'green':
+
+            return 'yellow'
+            break;
+
+        case 'yellow':
+
+            return 'red'
+            break;
+
+        case 'red':
+
+            return 'green'
+            break
+    }
+
+
+}
