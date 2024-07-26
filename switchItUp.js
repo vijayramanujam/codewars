6e02a3d4247e91aeaeb9a6f77c17c016337dcecf
Vijay Ramanujam <159724240+vijayramanujam@users.noreply.github.com>
2024-07-26 22:40:47 +0530

    Switch it Up!

---
+++ switchItUp.js
@@ -0,0 +1,20 @@
+function switchItUp(number) {
+    //Write your own Code!
+} function switchItUp(number) {
+
+    const nums = {
+
+        0: 'Zero',
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine'
+    }
+
+    return nums[number]
+}
